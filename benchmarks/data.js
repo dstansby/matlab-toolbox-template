6e0e83da55c2e29652c28dadcf245b70aa34439a
github-action-benchmark <github@users.noreply.github.com>
2022-09-22 15:30:26 +0000

    add Toolbox benchmarks (customSmallerIsBetter) benchmark result for f8c310fca10be5a3f102de4dcb9e7deada075ec6

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663860136800,
+  "lastUpdate": 1663860626561,
   "repoUrl": "https://github.com/dstansby/matlab-toolbox-template",
   "entries": {
     "Toolbox benchmarks": [
@@ -23,6 +23,62 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/dstansby/matlab-toolbox-template/commit/222708294a11bda473d75632a3b48ec7a471f05d"
         },
         "date": 1663860135973,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TestFixtures.defaultCurrentPoint",
+            "unit": "Seconds"
+          },
+          {
+            "name": "TestFixtures.defaultXLim",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value1,num2=value1)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value1,num2=value2)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value2,num2=value1)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value2,num2=value2)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testEqual",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testNotEqual",
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dstansby@gmail.com",
+            "name": "David Stansby",
+            "username": "dstansby"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f8c310fca10be5a3f102de4dcb9e7deada075ec6",
+          "message": "Merge pull request #23 from dstansby/benchmarks\n\nAdd benchmark saving and uploading",
+          "timestamp": "2022-09-22T16:27:55+01:00",
+          "tree_id": "212a2bb0b095e0947cee89633bfc69753d21d364",
+          "url": "https://github.com/dstansby/matlab-toolbox-template/commit/f8c310fca10be5a3f102de4dcb9e7deada075ec6"
+        },
+        "date": 1663860625746,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
