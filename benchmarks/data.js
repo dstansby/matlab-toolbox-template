c4d23075021f21f3e81cc37488d7c60f36cdf09b
github-action-benchmark <github@users.noreply.github.com>
2022-09-22 15:22:16 +0000

    add Toolbox benchmarks (customSmallerIsBetter) benchmark result for 222708294a11bda473d75632a3b48ec7a471f05d

---
+++ benchmarks/data.js
@@ -0,0 +1,64 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1663860136800,
+  "repoUrl": "https://github.com/dstansby/matlab-toolbox-template",
+  "entries": {
+    "Toolbox benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "dstansby@gmail.com",
+            "name": "David Stansby",
+            "username": "dstansby"
+          },
+          "committer": {
+            "email": "dstansby@gmail.com",
+            "name": "David Stansby",
+            "username": "dstansby"
+          },
+          "distinct": true,
+          "id": "222708294a11bda473d75632a3b48ec7a471f05d",
+          "message": "Fix artifact location",
+          "timestamp": "2022-09-22T15:20:42+01:00",
+          "tree_id": "212a2bb0b095e0947cee89633bfc69753d21d364",
+          "url": "https://github.com/dstansby/matlab-toolbox-template/commit/222708294a11bda473d75632a3b48ec7a471f05d"
+        },
+        "date": 1663860135973,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TestFixtures.defaultCurrentPoint",
+            "unit": "Seconds"
+          },
+          {
+            "name": "TestFixtures.defaultXLim",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value1,num2=value1)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value1,num2=value2)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value2,num2=value1)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value2,num2=value2)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testEqual",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testNotEqual",
+            "unit": "Seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
