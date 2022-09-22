efeb13be7ac35cb320a6cbfb1069380ddd476fae
github-action-benchmark <github@users.noreply.github.com>
2022-09-22 15:35:28 +0000

    add Toolbox benchmarks (customSmallerIsBetter) benchmark result for df261a5ab30413c93f9be223f39390c3774a1d03

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663860626561,
+  "lastUpdate": 1663860928158,
   "repoUrl": "https://github.com/dstansby/matlab-toolbox-template",
   "entries": {
     "Toolbox benchmarks": [
@@ -79,6 +79,62 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/dstansby/matlab-toolbox-template/commit/f8c310fca10be5a3f102de4dcb9e7deada075ec6"
         },
         "date": 1663860625746,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TestFixtures.defaultCurrentPoint",
+            "unit": "Seconds"
+          },
+          {
+            "name": "TestFixtures.defaultXLim",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value1,num2=value1)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value1,num2=value2)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value2,num2=value1)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=value2,num2=value2)",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testEqual",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testNotEqual",
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dstansby@gmail.com",
+            "name": "David Stansby",
+            "username": "dstansby"
+          },
+          "committer": {
+            "email": "dstansby@gmail.com",
+            "name": "David Stansby",
+            "username": "dstansby"
+          },
+          "distinct": true,
+          "id": "df261a5ab30413c93f9be223f39390c3774a1d03",
+          "message": "Only update benchmarks on main",
+          "timestamp": "2022-09-22T16:31:52+01:00",
+          "tree_id": "15b677ed3b79a3b1587deb0cadc3d864fdaec1af",
+          "url": "https://github.com/dstansby/matlab-toolbox-template/commit/df261a5ab30413c93f9be223f39390c3774a1d03"
+        },
+        "date": 1663860926392,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
