7859cef6c4c1ce291330692bbef324ceb5b07ee4
github-action-benchmark <github@users.noreply.github.com>
2022-10-03 14:11:58 +0000

    add Toolbox benchmarks (customSmallerIsBetter) benchmark result for e1c4cbe0da0d16610d45e2228af506eada1a7ea1

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664805698689,
+  "lastUpdate": 1664806318152,
   "repoUrl": "https://github.com/dstansby/matlab-toolbox-template",
   "entries": {
     "Toolbox benchmarks": [
@@ -287,6 +287,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "unit_test.testNotEqual",
             "value": "0.028569",
+            "unit": "Seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "dstansby@gmail.com",
+            "name": "David Stansby",
+            "username": "dstansby"
+          },
+          "committer": {
+            "email": "dstansby@gmail.com",
+            "name": "David Stansby",
+            "username": "dstansby"
+          },
+          "distinct": true,
+          "id": "e1c4cbe0da0d16610d45e2228af506eada1a7ea1",
+          "message": "Skip deploying docs to GH pages",
+          "timestamp": "2022-10-03T15:09:11+01:00",
+          "tree_id": "76003297e4e46791ce232eec4929c4e540f0d255",
+          "url": "https://github.com/dstansby/matlab-toolbox-template/commit/e1c4cbe0da0d16610d45e2228af506eada1a7ea1"
+        },
+        "date": 1664806317802,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "TestFixtures.defaultCurrentPoint",
+            "value": "4.6182",
+            "unit": "Seconds"
+          },
+          {
+            "name": "TestFixtures.defaultXLim",
+            "value": "3.0235",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=1,num2=3)",
+            "value": "0.16633",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=1,num2=4)",
+            "value": "0.009812",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=2,num2=3)",
+            "value": "0.008721",
+            "unit": "Seconds"
+          },
+          {
+            "name": "parametrized_test.testClass(num1=2,num2=4)",
+            "value": "0.007447",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testEqual",
+            "value": "0.035305",
+            "unit": "Seconds"
+          },
+          {
+            "name": "unit_test.testNotEqual",
+            "value": "0.031402",
             "unit": "Seconds"
           }
         ]
